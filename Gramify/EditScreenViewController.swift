70cd773b79b32cb34b99e81225f0053b833b7bf9
Isaac <isaacakalanne@gmail.com>
2020-02-16 14:37:14 +0000

    Added default functions for collection views, and added reuse identifiers for collection view cells. Also created a 'Data Stores' folder, and placed the FiltersDataStore and ModesDataStore classes in this folder.

--- Gramify/EditScreenViewController.swift
+++ Gramify/EditScreenViewController.swift
@@ -64,5 +64,37 @@ class EditScreenViewController: UIViewController {
             }
         }
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == modeSelectCollectionView {
+            return listOfModes.count
+        } else if collectionView == editSelectCollectionView {
+            return listOfFilters.count
+        } else {
+            return 0
+        }
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        var reuseIdentifier : String
+        if collectionView == modeSelectCollectionView {
+            reuseIdentifier = "modeSelectCell"
+        } else if collectionView == editSelectCollectionView {
+            reuseIdentifier = "editSelectCell"
+        } else {
+            reuseIdentifier = ""
+        }
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath)
+
+        cell.backgroundColor = UIColor.cyan
+
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Cell index is \(indexPath.item)!")
+    }
 
 }
